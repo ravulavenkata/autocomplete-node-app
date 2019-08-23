8810bd1f548503fc28b260a6a47789b109b8ca27
Venkata Krishna Ravula <ravulavenkata@gmail.com>
2019-08-23 11:19:08 -0700

    Create responseViewModel.js

---
+++ app/core/common/response/responseViewModel.js
@@ -0,0 +1,17 @@
+
+module.exports = async function (app){
+
+    const searchResponseViewModel = 
+    {
+        title,
+        original_language,
+        popularity,
+        release_date,
+        vote_average,
+        vote_count
+
+    };
+     
+
+
+}
